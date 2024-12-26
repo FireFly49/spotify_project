b8c29ee29e6a8f7760e7969b9c253c64b8c94e3b
FireFly49 <lalitsuresh195@gmail.com>
2024-12-26 15:31:49 +1000

    Added Home route and authentication (again cause forgot in previous push)

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from flask import Flask
-
-import spotipy
 import os
+from secret import client_id, client_secret, secret_key
+
+from flask import Flask, session, redirect
+
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import FlaskSessionCacheHandler
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
+redirect_uri = 'http://localhost:5000/callback'
+scope = 'playlist-read-private'
 
+cache_handler = FlaskSessionCacheHandler(session)
+sp_oauth = SpotifyOAuth(
+    client_id = client_id,
+    client_secret= client_secret,
+    redirect_uri=redirect_uri,
+    scope=scope,
+    cache_handler=cache_handler,
+    show_dialog=True
+) 
 
+sp = Spotify(auth_manager=sp_oauth)
+
+@app.route('/')
+def home():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+ 
 if __name__ == "__main__":
     app.run(debug=True)
 
