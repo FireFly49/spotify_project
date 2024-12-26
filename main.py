fe7ca81f5498450b29b96af8f051c4abd030a842
FireFly49 <lalitsuresh195@gmail.com>
2024-12-26 21:26:26 +1000

    Implemented getting user playlists

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from secret import client_id, client_secret, secret_key
 
-from flask import Flask, session, redirect
+from flask import Flask, session, redirect, url_for, request
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
@@ -30,7 +30,31 @@ def home():
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
- 
+    return redirect(url_for('get_playlists')) 
+
+@app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_playlists'))
+
+
+@app.route('/get_playlists')
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+
+    playlists = sp.current_user_playlists()
+    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
+    playlists_html = "<br>".join([f"{name}: {url}" for name, url in playlists_info])
+    return playlists_html
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
